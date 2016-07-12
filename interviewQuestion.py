047b318dbccb05fbf652ef9c0d5caa38a529e458
dataAlgorithms <195527444@qq.com>
2016-07-12 15:36:39 +0800

    reverse string without affecting special character

--- interviewQuestion.py
+++ interviewQuestion.py
@@ -18,12 +18,32 @@ def reverse_string(aString=None):
 
     print aString
 
-if __name__ == "__main__":
-    aString = "Ab,c,de!$"
-    reverse_string(aString)
+2. reverse string without affecting special character
+eg. 
+Input:   str = "Ab,c,de!$"
+Output:  str = "ed,c,bA!$"
+def reverse_string_without_affect_special_character(aString=None):
 
-if __name__ == "__main__":
-    aString = "Ab,c,de!$"
-    print 'Primary string:', aString
-    print '\rReverse string:'
-    reverse_string(aString)
+    if aString is None:
+        return
+
+    aString = [s for s in aString]
+
+    n = len(aString)
+    i = 0
+    j = n-1
+    while i < j:
+        if not aString[i].isalnum():
+            i += 1
+            continue
+        if not aString[j].isalnum():
+            j -= 1
+            continue
+
+        aString[i],aString[j]=aString[j],aString[i]
+        i += 1
+        j -= 1
+
+    aString = ''.join(aString)
+
+    print aString
