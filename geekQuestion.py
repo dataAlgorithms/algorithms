521c71ac8ff9959d16e28d974621523c54d9f73f
dataAlgorithms <195527444@qq.com>
2016-08-01 17:54:44 +0800

    sum bit diff

--- geekQuestion.py
+++ geekQuestion.py
@@ -466,3 +466,49 @@ def nthMagicNo(n):
 if __name__ == "__main__":
     for i in range(1, 6):
         nthMagicNo(i)
+
+13. Given an integer array of n integers, find sum of 
+bit differences in all pairs that can be formed from array elements.
+
+ Bit difference of a pair (x, y) is count of different bits at same positions in binary representations of x and y. 
+For example, bit difference for 2 and 7 is 2. 
+Binary representation of 2 is 010 and 7 is 111 
+( first and last bits differ in two numbers).
+
+Examples:
+
+Input: arr[] = {1, 2}
+Output: 4
+All pairs in array are (1, 1), (1, 2)
+                       (2, 1), (2, 2)
+Sum of bit differences = 0 + 2 +
+                         2 + 0
+                      = 4
+
+def sumBitDiff(arr):
+
+    ans = 0
+    n = len(arr)
+    
+    # traverse over all bits
+    for i in range(0, 32):
+        count = 0
+        for j in range(0, n):
+            if arr[j] & (1 << i):
+                count += 1
+
+        # add count * (n-count)*2 to the answer
+        ans += (count * (n - count) * 2)
+
+    return ans
+
+"""
+4
+26
+"""
+if __name__ == "__main__":
+    arr = [1, 2]
+    print sumBitDiff(arr)
+
+    arr = [1, 3, 7, 8]
+    print sumBitDiff(arr)
