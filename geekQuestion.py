ed95daaada7243c61da1a9dbdc50a8b3b408e21e
dataAlgorithms <195527444@qq.com>
2016-08-04 19:17:27 +0800

    Returns count of set bits present in all numbers

--- geekQuestion.py
+++ geekQuestion.py
@@ -593,3 +593,75 @@ if __name__ == "__main__":
     print ''
 
     numToAny(149, 8)
+
+16. Returns count of set bits present in all numbers from 1 to n
+'''
+Return position of leftmost set bit
+The rightmost position is considered as 0
+'''
+def getLeftmostBit(n):
+
+    m = 0
+    while n > 1:
+        n = n >> 1
+        m += 1
+
+    return m
+
+'''
+Given the position of previous leftmost set bit in n (
+or an upper bound on leftmost position) returns the new
+position of leftmost set bit in n
+'''
+def getNextLeftmostBit(n, m):
+    temp = 1 << m
+    while n < temp:
+        temp = temp >> 1
+        m -= 1
+
+    return m
+
+'''
+Returns count of set bits present in all numbers from 1 to n
+'''
+def countSetBits(n):
+
+    # Get the position of leftmost set bit in n, this will be
+    # used as an upper bound for next set bit function
+    m = getLeftmostBit(n)
+
+    # Use the position
+    return _countSetBits(n, m)
+
+'''
+recursive function used by ocuntSetBits
+'''
+def _countSetBits(n, m):
+
+    # Base case: if n is 0, then set bit count is 0
+    if n == 0:
+        return 0
+
+    # get position of next leftmost set bit
+    m = getNextLeftmostBit(n, m)
+
+    # if n is of the form 2^x-1, if n is like 1, 3, 7, 15, 31...etc
+    # then we are done
+    # since positions are considered starting from 0, 1 is added to m
+    if n == (1<<(m+1)) -1:
+        return (m+1)*(1<<m)
+
+    # update n for next recursive call
+    n = n - (1<<m)
+    return (n+1) + countSetBits(n) + m*(1<<(m-1))
+
+'''
+4
+9
+12
+13
+35
+'''
+if __name__ == "__main__":
+    for n in [3, 6, 7, 8, 17]:
+        print countSetBits(n)
