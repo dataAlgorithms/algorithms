52a2a7ab4dd1ed237c811e13743aa94170b08507
dataAlgorithms <195527444@qq.com>
2016-08-10 18:35:04 +0800

    Modular multiplicative inverse

--- geekQuestion.py
+++ geekQuestion.py
@@ -821,3 +821,104 @@ if __name__ == "__main__":
     p = 13
     print power(2, 5, 13)
     print (x**y)%p
+
+21. Modular multiplicative inverse
+Given two integers ‘a’ and ‘m’, find modular multiplicative inverse of ‘a’ under modulo ‘m’.
+
+The modular multiplicative inverse is an integer ‘x’ such that.
+
+ a x ≡ 1 (mod m) 
+The value of x should be in {0, 1, 2, … m-1}, i.e., in the ring of integer modulo m.
+
+The multiplicative inverse of “a modulo m” exists if and only if a and m are relatively prime (i.e., if gcd(a, m) = 1).
+
+Examples:
+
+Input:  a = 3, m = 11
+Output: 4
+Since (4*3) mod 11 = 1, 4 is modulo inverse of 3
+One might think, 15 also as a valid output as "(15*3) mod 11" 
+is also 1, but 15 is not in ring {0, 1, 2, ... 10}, so not 
+valid.
+
+Input:  a = 10, m = 17
+Output: 12
+Since (10*12) mod 17 = 1, 12 is modulo inverse of 3
+
+def nvModInverse(a, m):
+
+    a = a%m
+    for x in range(1, m):
+        if ((a*x) % m == 1):
+            return x
+
+def exModInverse(a, m):
+
+    '''
+    Works when m and a are coprime
+    '''
+    m0 = m
+    x0 = 0
+    x1 = 1
+
+    if m == 1:
+        return 0
+
+    while a > 1:
+        # q is quotient
+        q = a / m
+        t = m
+
+        # m is remainder now, 
+        m = a % m
+        a = t
+
+        t = x0
+        x0 = x1 - q * x0
+        x1 = t
+
+    # make x1 positive
+    if x1 < 0:
+        x1 += m0
+
+    return x1
+ 
+def feModInverse(a, m):
+
+    def gcd(a, b):
+        if a == 0:
+            return b
+
+        return gcd(b%a, a)
+
+    def power(x, y, m):
+        if y == 0:
+            return 1
+
+        p = power(x, y/2, m) % m
+        p = (p * p) % m
+
+        if y%2 == 0:
+            return p
+        else:
+            return (x * p) % m
+
+    g = gcd(a, m)
+    if g != 1:
+        print 'Inverse does not exit!'
+    else:
+        # if a and m are relatively prime, then modulo inverse
+        # is a^(m-2) mode m
+        return power(a, m-2, m)
+
+"""
+4
+4
+4
+"""
+if __name__ == "__main__":
+    a = 3
+    m = 11
+    print modInverse(a, m)
+    print exModInverse(a, m)
+    print feModInverse(a, m)
