3b54198ec965c79872c7c2fd502c8e80d2eabe66
dataAlgorithms <195527444@qq.com>
2016-08-15 18:35:07 +0800

    Convex Hull

--- geekQuestion.py
+++ geekQuestion.py
@@ -1056,3 +1056,101 @@ if __name__ == "__main__":
     n = 30
     SieveofEratosthenes(n)
     
+24. Convex Hull
+class Point:
+    # Init
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+'''
+Find orientation of ordered triplet (p, q, r)
+the function returns following values
+0 ..>p, q and r are colinear
+1 ..>clockwise
+2 ..>counterclockwise
+'''
+def orientation(p, q, r):
+
+    val = (q.y - p.y) * (r.x - q.x) - \
+            (q.x - p.x) * (r.y - q.y)
+
+    # colinear
+    if val == 0:
+        return 0
+
+    # clock or counterclock wise
+    if val > 0:
+        return 1
+    else:
+        return 2
+
+# Prints convex hull of a set of n points
+def convexHull(points, n):
+
+    # There must be at least 3 points
+    if n < 3:
+        return
+
+
+    # Initialize result
+    hull = []
+
+    # Find the leftmost point
+    l = 0
+    for i in range(l, n):
+        if points[i].x < points[l].x:
+            l = i
+    
+    '''
+    Start from leftmost point, keep moving counterclockwise
+    until reach the start point again.  This loop runs O(h)
+    times where h is number of points in result or output.
+    '''
+    p = l
+
+    while True:
+
+        # add current point to result
+        hull.append(points[p])
+
+        '''
+        Search for a point 'q' such that orientation(p, x,
+        q) is counterclockwise for all points 'x'. The idea
+        is to keep track of last visited most counterclock-
+        wise point in q. If any point 'i' is more counterclock-
+        wise than q, then update q.
+        '''
+        q = (p+1)%n
+        for i in range(0, n):
+            # If i is more counterclockwise than current q, then update q
+            if orientation(points[p], points[i], points[q]) == 2:
+                q = i
+        
+        '''
+        Now q is the most counterclockwise with respect to p
+        Set p as q for next iteration, so that q is added to
+        result 'hull'
+        '''
+        p = q
+
+        if p == l:
+            break
+
+    # Print result
+    for i in range(0, len(hull)):
+        print "(" + str(hull[i].x) + "," + str(hull[i].y) + ")"
+
+'''
+(0,3)
+(0,0)
+(3,0)
+(3,3)
+
+'''
+if __name__ == "__main__":
+    #points = [[0, 3], [2, 2], [1, 1], [2, 1], [3, 0], [0, 0], [3, 3]]
+    points = [Point(0, 3), Point(2,2), Point(1,1), Point(2,1), Point(3,0), Point(0,0), Point(3,3)]
+    n = len(points)
+    convexHull(points, n)
