2b72e64f86898c3a4fdd6faa23bec41e927c9a7c
dataAlgorithms <195527444@qq.com>
2016-09-05 18:37:35 +0800

    knapsack

--- geekQuestion.py
+++ geekQuestion.py
@@ -1692,3 +1692,80 @@ if __name__ == "__main__":
     subsetSum([2,6,14,8,9,15,-1,1,24,12,4,20,13,7,16],15)
 
 36. knapsack
+import collections
+import functools
+
+class Memoized:
+    '''
+    Decorator, caches a function's return value each item it is called
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable, a list, for instance
+            # better to not cache than blow up
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __repr__(self):
+        '''Return the funciton's docstring.'''
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        '''Support instance methods'''
+        return functools.partial(self.__call__, obj)
+
+def knapsack(items, maxweight):
+    """
+    Solve the knapsack problem by finding the most valuable
+    subsequence of items subject that weights no more than
+    maxweight
+
+    items is s sequence of pairs (value, weight) where value if
+    a number and weight is a non-negative integer.
+
+    maxweight is a non-negative integer.
+
+    return a pair whose first element is the sum of values in the most
+    valuable subsequence, and whose second elements is the subsequence.
+
+    >>> items = [(4, 12), (2, 1), (6, 4), (1, 1), (2, 2)]
+    >>> knapsack(items, 15)
+    (11, [(2, 1), (6, 4), (1, 1), (2, 2)])
+    """
+
+    # Returns the value of the most valuable subsequence of the first i
+    # elements in items whose weights sum to no more than j
+    @Memoized
+    def bestvalue(i, j):
+        if i == 0: return 0
+        value, weight = items[i-1]
+        if weight > j:
+            return bestvalue(i-1, j)
+        else:
+            return max(bestvalue(i-1, j),
+                 bestvalue(i-1, j-weight)+value)
+
+    j = maxweight
+    result = []
+    for i in xrange(len(items), 0, -1):
+        if bestvalue(i, j) != bestvalue(i-1,j):
+            result.append(items[i-1])
+            j -= items[i-1][1]
+    result.reverse()
+    return bestvalue(len(items), maxweight), result
+
+'''
+(11, [(2, 1), (6, 4), (1, 1), (2, 2)])
+'''
+if __name__ == "__main__":
+    items = [(4, 12), (2, 1), (6, 4), (1, 1), (2, 2)]
+    print knapsack(items, 15)
