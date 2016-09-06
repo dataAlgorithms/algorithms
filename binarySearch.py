15cb3bd0e4cbda850b39dea9c326a9b8c3399c48
dataAlgorithms <195527444@qq.com>
2016-09-06 19:13:18 +0800

    find all peak elements

--- binarySearch.py
+++ binarySearch.py
@@ -204,31 +204,22 @@ if __name__ == "__main__":
 '''
 def findPeak(arr):
 
-    tmpList = []
-
     n = len(arr)
     
     low = 0
     high = n -  1
     while low <= high:
         mid = (low + high) // 2
-        if mid == 0 and arr[mid+1] <= arr[mid]:
-            tmpList.append(arr[mid])
-            mid += 1
-        elif arr[mid-1] <= arr[mid] and mid == n -1:
-            tmpList.append(arr[mid])
-            break
-        elif arr[mid-1] <= arr[mid] and arr[mid+1] <= arr[mid]:
-            tmpList.append(arr[mid]
+        
+        if (mid == 0 or arr[mid-1] <= arr[mid]) and \
+             (mid == n-1 or arr[mid+1] <= arr[mid]):
+            return arr[mid]
         elif mid > 0 and arr[mid-1] > arr[mid]:
             high = mid -1
         else:
             low = mid + 1
 
-    if len(tmpList) != 0:
-        return tmpList
-    else:
-        return -1
+    return -1
 
 '''
 20
@@ -236,3 +227,49 @@ def findPeak(arr):
 if __name__ == "__main__":
     arr = [10,20,15,2,23,90,67]
     print findPeak(arr)
+    
+'''
+8. Find all peak elements
+'''
+def findAllPeak(A):
+
+    left = 0
+    right = len(A) - 1
+    tmpList = []
+    while left + 1 < right:
+        mid = left + (right - left)/2
+        if A[mid] > A[mid-1] and A[mid] > A[mid+1]:
+            tmpList.append(mid)
+            left = right
+            right = len(A) -1
+            continue
+        if A[mid] == left:
+            right = mid
+        elif A[mid] == right:
+            left = mid
+        elif A[mid] < A[left]:
+            right = mid
+        elif A[mid] < A[right]:
+            left = mid
+        elif A[mid] > A[mid+1]:
+            right = mid
+        else:
+            left = mid
+     
+    if A[left] > A[left-1] and A[left] > A[left+1]:
+        tmpList.append(left)
+    if A[right] > A[right-1] and A[right] > A[right+1]:
+        tmpList.append(right)
+        
+    if len(tmpList) != 0:
+        return tmpList
+    else:
+        return -1
+    
+'''
+20 90
+'''
+if __name__ == "__main__":
+    arr = [10,20,15,2,23,90,67]
+    for i in findAllPeak(arr):
+        print arr[i],
