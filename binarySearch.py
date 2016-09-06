4058aa678ecce11ba33f36de9e8073e828754b0d
dataAlgorithms <195527444@qq.com>
2016-09-06 16:57:40 +0800

    floor number

--- binarySearch.py
+++ binarySearch.py
@@ -83,3 +83,33 @@ if __name__ == "__main__":
     print binarySearchFewComparation(theSeq, target)
     end = time.clock()
     print 'end-start:', end-start
+
+'''
+4. Given an array of N distinct sorted integers, find floor value of input ‘key’. Say, A = {-1, 2, 3, 5, 6, 8, 9, 10} and key = 7, we should return 6 as outcome.
+'''
+def floor(aList, key):
+    
+    def toFloor(aList, l, r, key):
+    
+        while r-l > 1:
+            m = (l+r) // 2
+            if aList[m] <= key:
+                l = m
+            else:
+                r = m
+    
+        return aList[l]
+
+    # add error check if key < aList[0]
+    if key < aList[0]:
+        return -1
+
+    return toFloor(aList, 0, len(aList)-1, key)
+
+'''
+6
+'''
+if __name__ == "__main__":
+    aList = [-1, 2, 3, 5, 6, 8, 9, 10]
+    key = 7
+    print floor(aList, key)
