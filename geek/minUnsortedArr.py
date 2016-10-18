5325d6f84eeef3554e45c945a4ecf8be4e945e67
dataAlgorithms <195527444@qq.com>
2016-10-18 17:02:08 +0800

    Update minUnsortedArr.py

--- geek/minUnsortedArr.py
+++ geek/minUnsortedArr.py
@@ -18,13 +18,13 @@ def minimumUnSorted(theSeq):
         return
 
     # find the first right item which is less than the before item
-    for e in range(n-2, 0, -1):
+    for e in range(n-1, 0, -1):
         if theSeq[e] < theSeq[e-1]:
             break
 
     # find the min and max between s and e
     theMin = theSeq[s]
-    theMax = theSeq[e]
+    theMax = theSeq[s]
     for k in range(s+1, e+1):
         if theSeq[k] < theMin:
             theMin = theSeq[k]
@@ -49,9 +49,12 @@ def minimumUnSorted(theSeq):
 '''
 start= 3 end= 8
 [30, 25, 40, 32, 31, 35]
+start= 2 end= 5
+[15, 25, 6, 7]
 '''
 if __name__ == "__main__":
-    theSeq = [10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60]
-    start,end = minimumUnSorted(theSeq)
-    print "start=",start,"end=",end
-    print theSeq[start:end+1]
+    for theSeq in [[10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60], [0, 1, 15, 25, 6, 7, 30, 40, 50]]:
+
+        start,end = minimumUnSorted(theSeq)
+        print "start=",start,"end=",end
+        print theSeq[start:end+1]
