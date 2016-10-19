9a74916e7ef117c3dec4925499a9c12d664c76b6
dataAlgorithms <195527444@qq.com>
2016-10-19 14:28:26 +0800

    quickSort for single linked list

--- searchSort/quickSort.py
+++ searchSort/quickSort.py
@@ -230,3 +230,200 @@ if __name__ == "__main__":
         n = len(arr)
         quickSortIterative(arr, 0, n-1)
         print "Sorted array is:", arr
+
+'''
+5. QuickSort for Single LinkedList
+In partition(), we consider last element as pivot. We traverse through the current list and 
+if a node has value greater than pivot, we move it after tail. If the node has smaller value, 
+we keep it at its current position.
+
+In QuickSortRecur(), we first call partition() which places pivot at correct position 
+and returns pivot. After pivot is placed at correct position, we find tail node of 
+left side (list before pivot) and recur for left list. Finally, we recur for right list.
+'''
+# Implement Bag using linked list
+class LinkedList:
+    # Init
+    def __init__(self):
+        self._head = None
+        self._size = 0
+
+    # Length
+    def __len__(self):
+        return self._size
+
+    # Contain
+    def __contains__(self, target):
+        curNode = self._head
+        while curNode is not None and curNode.data != target:
+            curNode = curNode.next
+
+        return curNode is not None
+
+    # Add
+    def add(self, element):
+        newItem = LinkedListNode(element)
+        newItem.next = self._head
+        self._head = newItem
+        self._size += 1
+
+    # Remove
+    def remove(self, element):
+        preNode = None
+        curNode = self._head
+        while curNode is not None and curNode.data != element:
+            preNode = curNode
+            curNode = curNode.next
+
+        assert curNode is not None, "element is not in the Bag."
+        self._size -= 1
+
+        if curNode is self._head:
+            self._head = curNode.next
+        else:
+            preNode.next = curNode.next
+
+        return curNode.data
+
+    # Iter
+    def __iter__(self):
+        return LinkedListBagIterator(self._head)
+
+# Bag storage
+class LinkedListNode:
+    # Init
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+# Bag iter
+class LinkedListBagIterator:
+    # Init
+    def __init__(self, head):
+        self.curNode = head
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.curNode is not None:
+            value = self.curNode.data
+            self.curNode = self.curNode.next
+            return value
+        else:
+            raise StopIteration
+
+# Returns the last node of the list
+def getTail(curNode):
+
+    while curNode is not None and curNode.next is not None:
+        curNode = curNode.next
+
+    return curNode
+
+# Partitions the list taking the last element as the pivot
+def partition(head, end, newHead, newEnd):
+
+    pivot = end
+    prev = None
+    cur = head
+    tail = pivot
+
+    # During partition, both the head and end of the list might change
+    # which is updated in the newHead and newEnd variables
+    while cur != pivot:
+        if cur.data < pivot.data:
+            # First node htat has a value less than the pivot
+            # becomes the new head
+            if newHead is None:
+                newHead = cur
+
+            prev = cur
+            cur = cur.next
+        else: 
+            # If cur node is greater than pivot
+            # Move cur node to next of tail, and change tail
+            if prev is not None:
+                prev.next = cur.next
+            tmp = cur.next
+            cur.next = None
+            tail.next = cur
+            tail = cur
+            cur = tmp
+
+    # If the pivot data is the smallest element in the current list
+    # pivot becomes the head
+    if newHead is None:
+        newHead = pivot
+
+    # Update newEnd to the current last node
+    newEnd = tail
+
+    # Return the pivot, newHead, newEnd
+    return pivot, newHead, newEnd
+
+# Here the sorting happends exclusive of the end node
+def quickSortRecur(head, end):
+
+    # Base condition
+    if head is None or head == end:
+        return head
+
+    newHead = None
+    newEnd = None
+
+    # Partition the list, newHead and newEnd will be updated
+    # by the partition function
+    pivot, newHead, newEnd = partition(head, end, newHead, newEnd)
+
+    # If pivot is the smallest element 
+    # no need to recur for the left part
+    if newHead != pivot:
+        # Set the node before the pivot node as None
+        tmp = newHead
+        while tmp.next != pivot:
+            tmp = tmp.next
+        tmp.next = None
+
+        # Recur for the list before pivot
+        newHead = quickSortRecur(newHead, tmp)
+
+        # Change next of last node of the left half to pivot
+        tmp = getTail(newHead)
+        tmp.next = pivot
+
+    # Recur for the list after the  pivot element
+    pivot.next = quickSortRecur(pivot.next, newEnd)
+
+    return newHead
+
+# Main function for quick sort 
+# Wrapper over recursive function quickSortRecur
+def quickSort(headRef):
+
+    newHead = quickSortRecur(headRef, getTail(headRef))
+    return newHead
+
+'''
+...Sort before
+ECON-111 HIST-350 ECON-101 HIST-340 MATH-121 CSCI-112 
+
+...Sort after
+CSCI-112 ECON-101 ECON-111 HIST-340 HIST-350 MATH-121
+'''
+if __name__ == "__main__":
+    smith = LinkedList()
+    smith.add('CSCI-112')
+    smith.add('MATH-121')
+    smith.add('HIST-340')
+    smith.add('ECON-101')
+    smith.add('HIST-350')
+    smith.add('ECON-111')
+    
+    print '...Sort before'
+    for item in smith:
+        print item,
+        
+    newsmith = quickSort(smith._head)
+    print '\r\r...Sort after'
+    while newsmith is not None:
+        print newsmith.data,
+        newsmith = newsmith.next
