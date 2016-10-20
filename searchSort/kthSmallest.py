449c91be82cf4f00f72523e6490c0a0e2c0edbec
dataAlgorithms <195527444@qq.com>
2016-10-20 18:12:36 +0800

    Create kthSmallest.py

---
+++ searchSort/kthSmallest.py
@@ -0,0 +1,59 @@
+# This function returns kth smallest element in distinct arr[l..r]
+# using quickSort based method,
+# Assumption: elements in arr[] are distinct
+def kthSmallest(arr, l, r, k):
+
+    import random
+
+    # standard partion process of quickSort
+    # it considers the last element as pivot
+    # and moves all smaller element to left of it and
+    # greater elements to right, 
+    # the function is used by randomPartition()
+    def partition(arr, l, r):
+        x = arr[r]
+        i = l
+
+        for j in range(l, r):
+            if arr[j] <= x:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+
+        arr[i],arr[r] = arr[r], arr[i]
+        return i
+
+    # Picks a random pivot element between l and r and partitions
+    # arr[l..r] arount the randomly picked element using partition
+    def randomPartition(arr, l, r):
+        n = r - l + 1
+        pivot = random.randrange(n)
+        arr[l+pivot],arr[r] = arr[r],arr[l+pivot]
+        return partition(arr, l, r) 
+
+    # If k is smaller than number of elements in array
+    if k > 0 and k <= r - l + 1:
+        # Partion the array around a random element and
+        # get position of pivot element in sorted array
+        pos = randomPartition(arr, l, r)
+
+        # If position is the same as k
+        if pos-l == k-1:
+            return arr[pos]
+
+        # If position is more, recur for left subarray
+        if pos-l > k-1:
+            return kthSmallest(arr, l, pos-1, k)
+
+        # Else recur for right subarray
+        return kthSmallest(arr, pos+1, r, k-pos+l-1)
+
+    return -1
+
+'''
+7
+10
+5
+'''
+if __name__ == "__main__":
+    for arr,k in [[[7, 10, 4, 3, 20, 15], 3], [[7, 10, 4, 3, 20, 15], 4], [[12, 3, 5, 7, 4, 19, 26], 3]]:
+        print kthSmallest(arr, 0, len(arr)-1, k)
