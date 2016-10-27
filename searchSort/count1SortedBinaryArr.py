00dd288caa26a08a3f30e835594a0bbcd17e3225
dataAlgorithms <195527444@qq.com>
2016-10-27 10:43:11 +0800

    Count 1’s in a sorted binary array

--- searchSort/count1SortedBinaryArr.py
+++ searchSort/count1SortedBinaryArr.py
@@ -39,3 +39,47 @@ if __name__ == "__main__":
                    [1, 1, 1, 1, 1, 1, 1],
                    [0, 0, 0, 0, 0, 0, 0]]:
         count1BinaryArr(theSeq)
+        
+'''
+Method Two: Optimize
+
+Count 1’s in a sorted binary array
+Given a binary array sorted in non-increasing order, count the number of 1’s in it.
+Examples:
+Input: arr[] = {1, 1, 0, 0, 0, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 1, 1, 1, 1, 1, 1}
+Output: 7
+
+Input: arr[] = {0, 0, 0, 0, 0, 0, 0}
+Output: 0
+
+We can use Binary Search to find count in O(Logn) time. The idea is to look for last
+ occurrence of 1 using Binary Search.
+ Once we find the index last occurrence, we return index + 1 as count.
+'''
+def count1SortedArr(theSeq, low, high):
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if (mid == high and theSeq[mid] == 1) or (theSeq[mid] == 1 and theSeq[mid+1] == 0):
+            return mid+1
+        if theSeq[mid] == 1:
+            return count1SortedArr(theSeq, mid+1, high)
+        else:
+            return count1SortedArr(theSeq, low, mid-1)
+
+    return 0
+
+'''
+2
+7
+0
+'''
+if __name__ == "__main__":
+    for theSeq in [[1, 1, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 1, 1, 1],
+                   [0, 0, 0, 0, 0, 0, 0]]:
+        print count1SortedArr(theSeq, 0, len(theSeq)-1)
