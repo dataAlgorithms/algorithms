03f2a9799df793caaae5ec83bd8a04bafa8433ea
dataAlgorithms <195527444@qq.com>
2016-10-28 16:26:20 +0800

    Update floorSearch.py

--- searchSort/floorSearch.py
+++ searchSort/floorSearch.py
@@ -65,3 +65,55 @@ if __name__ == "__main__":
         else:
             print "floor of %d is %d" % (k, arr[index])
         
+'''
+Method 2 (Efficient)
+
+The idea is to use Binary Search.
+Time Complexity : O(Log n)
+'''
+def floorSearch(arr, low, high, x):
+
+    # if low and high cross each other
+    if low > high:
+        return -1
+
+    # if last element is smaller than x
+    if x >= arr[high]:
+        return high
+
+    # find the middle point
+    mid  = (low+high) // 2
+
+    # if middle point is floor
+    if arr[mid] == x:
+        return mid
+
+    # If x lies between mid-1 and mid
+    if mid > 0 and arr[mid-1] <= x and x < arr[mid]:
+        return mid-1
+
+    # If x is smaller than mid, floor must be in left half
+    if x < arr[mid]:
+        return floorSearch(arr, low, mid-1, x)
+    else:
+        return floorSearch(arr, mid+1, high, x)
+
+'''
+floor of 7 is 6
+floor of 5 is 2
+floor of 20 is 19
+floor of 0 doesnot exit in array
+'''
+if __name__ == "__main__":
+    for arr,k in [[[-1, 2, 3, 5, 6, 8, 9, 10],7],
+                  [[1, 2, 8, 10, 10, 12, 19],5],
+                  [[1, 2, 8, 10, 10, 12, 19],20],
+                  [[1, 2, 8, 10, 10, 12, 19],0]]:
+
+        low = 0
+        high = len(arr) - 1
+        index = floorSearch(arr, low, high, k)
+        if index == -1:
+            print "floor of %d doesnot exit in array" % k
+        else:
+            print "floor of %d is %d" % (k, arr[index])
