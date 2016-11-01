ff7001739f980eb98366e442c39b6780f2a48bdd
dataAlgorithms <195527444@qq.com>
2016-11-01 15:42:58 +0800

    Update radixSort.py

--- searchSort/radixSort.py
+++ searchSort/radixSort.py
@@ -24,7 +24,8 @@ Example:
 Original, unsorted list:
 
 170, 45, 75, 90, 802, 24, 2, 66
-Sorting by least significant digit (1s place) gives: [*Notice that we keep 802 before 2, because 802 occurred before 2 in the original list, and similarly for pairs 170 & 90 and 45 & 75.]
+Sorting by least significant digit (1s place) gives: [*Notice that we keep 802 before 2, because 802 occurred
+before 2 in the original list, and similarly for pairs 170 & 90 and 45 & 75.]
 
 170, 90, 802, 2, 24, 45, 75, 66
 Sorting by next digit (10s place) gives: [*Notice that 802 again comes before 2 as 802 comes before 2 in the previous list.]
@@ -37,7 +38,8 @@ Sorting by most significant digit (100s place) gives:
 What is the running time of Radix Sort?
 Let there be d digits in input integers. Radix Sort takes O(d*(n+b)) time where b is the base for representing numbers,
 for example, for decimal system, b is 10. What is the value of d? If k is the maximum possible value, then d would be O(logb(k)).
-So overall time complexity is O((n+b) * logb(k)). Which looks more than the time complexity of comparison based sorting algorithms for a large k.
+So overall time complexity is O((n+b) * logb(k)). Which looks more than the time complexity of comparison based sorting algorithms 
+for a large k.
 
 Let us first limit k. Let k <= nc where c is a constant. In that case, the complexity becomes O(nLogb(n)).
 But it still doesn’t beat comparison based sorting algorithms.
@@ -46,7 +48,8 @@ If we set b as n, we get the time complexity as O(n). In other words, we can sor
 with range from 1 to nc if the numbers are represented in base n (or every digit takes log2(n) bits).
 
 Is Radix Sort preferable to Comparison based sorting algorithms like Quick-Sort?
-If we have log2n bits for every digit, the running time of Radix appears to be better than Quick Sort for a wide range of input numbers.
+If we have log2n bits for every digit, the running time of Radix appears to be better than Quick Sort for a wide range of input 
+numbers.
 The constant factors hidden in asymptotic notation are higher for Radix Sort and Quick-Sort uses hardware caches more effectively.
 Also, Radix sort uses counting sort as a subroutine and counting sort takes extra space to sort numbers.
 '''
