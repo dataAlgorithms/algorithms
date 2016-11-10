6cb394c1730656f25784ff23b370517e4a36ef00
dataAlgorithms <195527444@qq.com>
2016-11-10 15:24:34 +0800

    Update countOccurencesSortedArr.py

--- searchSort/countOccurencesSortedArr.py
+++ searchSort/countOccurencesSortedArr.py
@@ -1,35 +1,54 @@
+#! coding=utf-8
+
 '''
 Problem Statement: Given a sorted array with possible duplicate elements.
- Find number of occurrences of input key in log N time
+Find number of occurrences of input key in log N time.
+
+The idea here is finding left and right most occurrences of key in the array using binary search. 
+We can modify floor function to trace right most occurrence and left most occurrence. 
 '''
-def getRightPosition(aList, l, r, key):
+def occurenceCount(theSeq, target):
 
-    while r -l > 1:
-        m = (l+r) // 2
-        if aList[m] <= key:
-            l = m
-        else:
-            r = m
+    '''
+    Input: Indices ragne [l...r)
+    Invariant: theSeq[l] <= target and theSeq[r] > target
+    '''
+    def getRightPosition(theSeq, l, r, target):
+        
+        while r-l > 1:
+            m = l + (r -l)/2
 
-    return l
+            if theSeq[m] <= target:
+                l = m
+            else:
+                r = m
 
-def getLeftPosition(aList, l, r, key):
+        return l
 
-    while r - l > 1:
-        m = (l+r) // 2
-        if aList[m] >= key:
-            r = m
-        else:
-            l = m
+    '''
+    Input: Indices Range (l...r]
+    Invariant: theSeq[r] >= target and theSeq[l] > target
+    '''
+    def getLeftPosition(theSeq, l, r, target):
+        
+        while r-l > 1:
+            m = l + (r-l) /2
+          
+            if theSeq[m] >= target:
+                r = m
+            else:
+                l = m
 
-    return r
+        return r
 
-def countOccurences(aList, key):
-    left = getLeftPosition(aList, -1, len(aList)-1 , key)
-    right = getRightPosition(aList, 0, len(aList), key)
+    # observe boundary conditions
+    left = getLeftPosition(theSeq, -1, len(theSeq)-1, target)
+    right = getRightPosition(theSeq, 0, len(theSeq), target)
 
-    if aList[left] == key and key == aList[right]:
-        return right - left + 1
+    # what if the element doesnot exists in the array
+    # the checks helps to trace that element exists
+    if theSeq[left] == target and target == theSeq[right]:
+        return right-left+1
     else:
         return 0
 
@@ -40,6 +59,6 @@ def countOccurences(aList, key):
 '''
 if __name__ == "__main__":
     aList = [-1, 2, 3, 5, 6, 6, 8, 9, 10]
-    print countOccurences(aList, 6)
-    print countOccurences(aList, 11)
-    print countOccurences(aList, 2)
+    print occurenceCount(aList, 6)
+    print occurenceCount(aList, 11)
+    print occurenceCount(aList, 2)
