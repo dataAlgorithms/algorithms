ec9d7c5d0c5e8db5126f29c310e4f49dff71bf9e
dataAlgorithms <195527444@qq.com>
2016-11-30 13:58:10 +0800

    Update insertSort.py

--- searchSort/insertSort.py
+++ searchSort/insertSort.py
@@ -1,7 +1,26 @@
 '''
 1.  Insert sort
 
- O(n*n)
+
+Time Complexity: O(n*n)
+Auxiliary Space: O(1)
+Boundary Cases: Insertion sort takes maximum time to sort if
+ elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+Algorithmic Paradigm: Incremental Approach
+Sorting In Place: Yes
+Stable: Yes
+Online: Yes
+Uses: Insertion sort is used when number of elements is small. 
+It can also be useful when input array is almost sorted, only few
+ elements are misplaced in complete big array.
+What is Binary Insertion Sort?
+We can use binary search to reduce the number of comparisons in
+ normal insertion sort. Binary Insertion Sort find use binary search 
+to find the proper location to insert the selected item at each iteration. 
+In normal insertion, sort it takes O(i) (at ith iteration) in worst case. 
+we can reduce it to O(logi) by using binary search. The algorithm as a whole
+ still has a running worst case running time of O(n2) because of the series 
+of swaps required for each insertion.
 '''
 def insertSort(theSeq):
     
