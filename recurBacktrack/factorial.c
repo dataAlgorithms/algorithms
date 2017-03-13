9b3bff0b4b1aa6b1151f2c345dcc7f8713e76f05
dataAlgorithms <195527444@qq.com>
2017-03-14 06:46:27 +0800

    Update factorial.c

--- recurBacktrack/factorial.c
+++ recurBacktrack/factorial.c
@@ -29,3 +29,45 @@ double facIteration(unsigned int n) {
 
     return facResult;
 }
+
+/*
+Test 
+
+/*
+ * helloWorld.c
+ *
+ *  Created on: 2017年3月13日
+ *      Author: baobao
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//int helloWorld(void);
+double facRecursive(unsigned int n);
+double facIteration(unsigned int n);
+
+int main(void) {
+	//helloWorld();
+
+	//Calculate factorial using recursive way
+	printf("%lf ", facRecursive(0));
+	printf("%lf ", facRecursive(1));
+	printf("%lf ", facRecursive(10));
+	printf("%lf\n", facRecursive(1000));
+
+	//Calculate factorial using loop way
+	printf("%lf ", facIteration(0));
+	printf("%lf ", facIteration(1));
+	printf("%lf ", facIteration(10));
+	printf("%lf\n", facIteration(1000));
+
+	return 1;
+}
+
+
+1.000000 1.000000 3628800.000000 1.#INF00
+1.000000 1.000000 3628800.000000 1.#INF00
+
+*/
