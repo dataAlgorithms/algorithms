4981f3b13d7883f983389e31b9582e855d190e48
dataAlgorithms <195527444@qq.com>
2017-03-14 06:41:08 +0800

    Calculate factorial using recursive and loop way

---
+++ recurBacktrack/factorial.c
@@ -0,0 +1,31 @@
+/*
+ * factory.c
+ *
+ *  Created on: 2017年3月14日
+ *      Author: baobao
+ */
+
+//Calculate factorial of positive integer using recursive way
+double facRecursive(unsigned int n) {
+
+    if(n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * facRecursive(n-1);
+    }
+}
+
+//Calculate factorial of positive integer using loop way
+double facLoop(unsigned int n) {
+
+    double facResult = 1.0;
+    int i;
+
+    if(n > 1) {
+        for(i=2; i<=n; i++) {
+            facResult *= i;
+        }
+    }
+
+    return facResult;
+}
